431537cffb3e354298c9267dee05c4d2d77ba86b
Cristian <cris.munoz0509@gmail.com>
2022-01-10 23:05:28 -0600

    update to website

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const tl = gsap.timeline({defaults: {ease: 'sine.out'} });
 tl.to(".text", {y:"0%", duration: 1.3, stagger: 0.3 });
 tl.to(".slider", {y: "-100%", duration: 1.5, delay: 1 });
 tl.to(".intro", {y: "-100%", duration: 1}, "-=1.1");
-tl.fromTo("nav", {opacity: .1, y:"-100"}, {y:"0", opacity: 1, duration: 1.5});
+tl.fromTo(".nav", {opacity: .1, y:"-100"}, {y:"0", opacity: 1, duration: 1.5});
 tl.fromTo(".big-text", {opacity: 0, y:"100"}, {y:"0", opacity: 1, duration: 1.5}, "-=1.5");
-tl.to(".logo", {rotation:1080, duration: 1})
+tl.to(".nav-menu li:first-child", {rotation:1080, duration: 1})
