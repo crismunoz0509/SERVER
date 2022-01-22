97b5cd38aa775a8aa4467545efb2a5d362a17886
Cristian <cris.munoz0509@gmail.com>
2022-01-22 14:50:52 -0600

    update

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const linesOfCode = ["var Sansa = mislead(Stark,( x ) / -rule(rule(-0.26) - -mislead(protect(TABLE[( -bar(( mislead(Arya + Sansa * rule(betray(COLS),( Sansa )) * --30 / Arya * ( rule() ),ROWS) / protect(90 * -destroy(ROWS,dog - 1 + rule(--0.35,43 - -( 97 ) - 1 - mislead(( Sansa ),( ( destroy(protect(Ygritte + ( COLS ) * ROWS,( betray(dog", "COLS - -0.05,( destroy(COLS) )) ) ) ),rule(-0.18 - ( 28 ),ROWS,dog) + rule(COLS,210.991))][destroy(protect(-130.967 * -87) + ( -( ( TABLE[--Sansa][y] ) ) )) / Hodor]]] : What do we say to the Lord of Death? var Hodor = y var Jon = TABLE[TABLE[TABLE[Arya][ROWS]][Hodor]][( TABLE[0.5][( ( rule(ROWS) ) )] )] var Stark = ( -COLS ) def mislead(){ y += -bar(destroy(-0.39 - COLS - --Hodor))","( bar(Sansa) ); Arya *= -( 0.93 ); Ygritte /= protect assert Hodor : 'Nothing burns like the cold.' assert ROWS : 'One can not simply parse himself' assert -TABLE[-0.66][destroy(( --0.68 ),COLS) * TABLE[betray(Stark)]["];
-const random_glitch_text = "@#$%&*";
+const linesOfCode = ["<html lang=en class=background><head> <meta charset=UTF-8> <meta http-equiv=X-UA-Compatible content=IE=edge> <meta name=viewport content=width=device-width", 
+                     "", ""];
+const random_glitch_text = ["@", "#", "!", "$", "%", "*", "&", "WELCOME", "HELLO", "(^", "01", "./", "_", "'", "//", "{}", "[]", "+", "-", "?", ">", "<", ","];
 const rainbowArray = [ 'rgb(255,0,0)',
 'rgb(255,0,42.5)',
 'rgb(255,0,85)',
@@ -43,10 +44,6 @@ function getRandomInt(max) {
 }
 
 function randomCodeLines(numOfLines){
-
-    var txt = 'var Sansa = mislead(Stark,( x ) / -rule(rule(-0.26) - -mislead(protect(TABLE[( -bar(( mislead(Arya + Sansa * rule(betray(COLS),( Sansa )) * --30 / Arya * ( rule() ),ROWS) / protect(90 * -destroy(ROWS,dog - 1 + rule(--0.35,43 - -( 97 ) - 1 - mislead(( Sansa ),( ( destroy(protect(Ygritte + ( COLS ) * ROWS,( betray(dog,( -1 )) )),TABLE[-Sansa][( Jon )] + -10 * -bar(TABLE[-31][( --Jon * (-betray(ROWS - Hodor,-foo(TABLE[foo(ROWS / -1)][ROWS])) ) )],Arya,-( -COLS / foo(--COLS * betray() * -60 - ( COLS / -190.73 )) )) + ( rule(--Sansa,-( COLS ),-TABLE[Jon][( Hodor )]) ) - Ygritte) ) ) / ( bar(( rule(TABLE[---0.41][-( --1 )],protect(( ( Hodor ) ))) ) / TABLE[foo()][TABLE[Ygritte][--46]],ROWS) )))),betray(ROWS,-Ygritte,mislead())) / TABLE[-0.57][58] ) * TABLE[COLS][( -Jon )] + COLS + -Sansa) / rule(-rule(-foo(-( COLS ))),-( dog ) / bar(Sansa)) )][-foo(-0.73 - ( 63 ))])) - -betray(83,( ( bar(ROWS,( ROWS )) ) ),Jon * Sansa) - ( 1 ),dog,dog),-( dog ) * ---300.02 + dog) def rule(dog,dog){ x; y -= 1 } def TABLE[x][j] { 75 } assert ( ( -protect() ) ) :  One can not simply parse himself  def foo(Hodor){ -Stark; COLS - ROWS } assert betray(COLS) :  Winter  is coming assert  ( ROWS ) :  A mind needs books as a sword needs a whetstone, if it is to keep its edge.  def protect(Ygritte){ Stark *= -86 } assert ( -550.326 ) - TABLE[( COLS )][Arya * -dog] - 80 / Stark :  A mind needs books as a sword needs a whetstone, if it is to keep its edge.  def TABLE[( betray (-430.408,Stark + bar(Stark,COLS) + ( COLS ) - ( betray(--0.55) ),60.235))) )][k] { Stark /= mislead(x) } var Hodor = -780.817 / Sansa - rule(0.63) def TABLE[rule(( -Hodor ),-( ROWS ))][i] { Hodor *= y; dog -= Ygritte } var Ygritte = -ROWS + ( -Stark / dog ) var x = -mislead(ROWS) def TABLE[-bar(--0.76,Stark,COLS)][k] { 89 } assert -67 :  Nothing burns like the cold.  def TABLE[( -( rule(-( x ) + Ygritte + rule(betray(COLS,Jon - protect(COLS) - --Ygritte * x),mislead()) * x) ) + dog ) * 520.81][i] { if(dog - -( ( x + rule(Hodor,( betray() )) ) ) / -betray(rule(mislead(rule(-0.35,COLS),( bar(rule(ROWS - ( betray(-0.06,-( -TABLE[1][150.5] )) )),Ygritte)  )),( -foo() )) + -y)){ foo(foo(ROWS,-80,-0.08),Ygritte / ( -0.37 )) } else { if(Arya / y * -rule(foo(betray(( -betray(COLS,TABLE[( Stark ) * Sansa][betray(Stark,ROWS,protect(83,y)) + 0.71],-COLS) ),-destroy(-TABLE[ROWS][destroy(-100,COLS,dog)]),ROWS * Hodor + 3)),bar(Arya - protect() * COLS * TABLE[foo(-( ( -( ROWS ) ) ))\
-        ya /= ( bar(Sansa) ); Arya *= -( 0.93 ); Ygritte /= protect assert Hodor : "Nothing burns like the cold." assert ROWS : "One can not simply parse himself" assert -TABLE[-0.66][destroy(( --0.68 ),COLS) * TABLE[betray(Stark)][betray(Arya,mislead(mislead(( Ygritte ),dog,COLS),betray() / mislead() + x,-0.9)) / TABLE[-destroy(( foo(-51 / ( COLS ) * COLS,Sansa) ) / ( ( ( mislead(-COLS - -0.05,( destroy(COLS) )) ) ) ),rule(-0.18 - ( 28 ),ROWS,dog) + rule(COLS,210.991))][destroy(protect(-130.967 * -87) + ( -( ( TABLE[--Sansa][y] ) ) )) / Hodor]]] : "What do we say to the Lord of Death?" var Hodor = y var Jon = TABLE[TABLE[TABLE[Arya][ROWS]][Hodor]][( TABLE[0.5][( ( rule(ROWS) ) )] )] var Stark = ( -COLS ) def mislead(){ y += -bar(destroy(-0.39 - COLS - --Hodor)) * -49 }';
-
     let clear_ul = document.getElementById("coding-text");
     clear_ul.innerHTML = "";
 
@@ -56,7 +53,7 @@ function randomCodeLines(numOfLines){
         var li = document.createElement("li");
         li.setAttribute('id','hackli');
 
-        let element = linesOfCode[getRandomInt(3)];
+        let element = linesOfCode[getRandomInt(1)];
         for(let i = 0; i < element.length; i++){
             var pixel_len = 0;
             if (screen.width >= 1100) {
@@ -77,32 +74,6 @@ function randomCodeLines(numOfLines){
     }
     
 }
-
-function typeWrite(span_position){
-
-    var ul = document.getElementById("coding-text");
-    var li = document.createElement("li");
-    li.setAttribute('id','hackli');
-
-    let element = linesOfCode[getRandomInt(3)];
-    for(let i = 0; i < element.length; i++){
-        var pixel_len = 0;
-        if (screen.width >= 1100) {
-            pixel_len = 13;
-        } else {
-            pixel_len = 9;
-        }
-        var width_of_ul = ul.clientWidth;
-        var number_of_spans = width_of_ul / pixel_len;
-        if(i < number_of_spans){
-            li.innerHTML += "<span>" + element[i] +"</span>";
-        }
-    }
-
-
-    ul.appendChild(li);
-}
-
 //activate landing page
 startTL.to(".text", {y:"0%", duration: 1.5, ease: 'sine.out'});
 function activateLanding(){
@@ -138,7 +109,7 @@ function hackerText() {
         line_scroll_time = screen.width / 550;
         
     }else{
-        line_scroll_time = screen.width / 100;
+        line_scroll_time = screen.width / 90;
     }
 
     codeLinesTL.from(spanArr, {duration: line_scroll_time, ease: "none", stagger: .01, text: {value: ""}}, "-=2.8");
@@ -158,7 +129,7 @@ gsap.registerEffect({
             }else{
                 nextColor = 0;
             }
-        },color: rainbowArray[nextColor], ease:"none", duration: 0.7, yoyo: true, repeat: 1, text:{value:random_glitch_text[getRandomInt(4)], delimiter:""}})
+        },color: rainbowArray[nextColor], ease:"none", duration: 0.7, yoyo: true, repeat: 1, text:{value:random_glitch_text[getRandomInt(random_glitch_text.length)], delimiter:""}})
     }
 });
 
